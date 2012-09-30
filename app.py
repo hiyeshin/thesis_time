bc402d84e6ce301158c2bd5d4fb1391ad36af18a
Hiye Shin <hiyeshin@gmail.com>
2012-09-29 20:48:58 -0400

    first draft

---
+++ app.py
@@ -0,0 +1,105 @@
+#######################################
+######## week 4 thesis time ###########
+########################################
+
+import os, datetime
+
+from flask import Flask, request # Retrieve Flask, our framework
+from flask import render_template
+
+app = Flask(__name__)   # create our flask app
+
+# thesis type category
+categories = {}
+#Let's make a dictionary
+categories["Performance"] = {
+	"image": "performance.gif",
+	"title": "Performance",
+	"inStock": True
+}
+
+categories["Installation"] = {
+	"image": "installation.gif", 
+	"title": "Installation",
+	"inStock": True
+}
+
+categories["Web"] = {
+	"image": "web.gif",
+	"title": "Web",
+	"inStock": True
+}
+
+categories["Mobile app"] = {
+	"image": "mobile.gif",
+	"title": "Mobile app",
+	"inStock": True
+}
+
+categories ["Extra"] = {
+	"image": "extra.gif",
+	"title": "Extra",
+	"inStock": True
+}
+
+# this is our main page
+@app.route("/")
+def index():
+	# render the template, pass in the category dictionary to it as "categories"
+	return render_template("main.html", categories=categories)
+
+
+# this is the 2nd route - can be access with /choose
+@app.route("/choose", methods=["POST"])
+def choose():
+
+	# Get the user submitted data
+	chooseData = {
+		"name": request.form.get("name"),
+		"category_name": request.form.get("categories"),
+		"start_time": request.form.get("start_time")
+	}
+	
+	# get the category data by category_name
+	chooseData["category"] = categories[chooseData.get("category_name")]
+	
+	# determine starting date
+	now = datetime.datetime.now()
+	
+	if chooseData["start_time"] != "dunno":
+		now = datetime.datetime.now()
+		
+		if chooseData["start_time"] == "already":
+			future = datetime.timedelta(days = 90) # I'll change it later
+			
+		elif chooseData["start_time"] == "now":
+			future = datetime.timedelta(days = 90) # I'll change it later
+			
+		elif chooseData["start_time"] =="midterm":
+			future = datetime.timedelta(days = 45) #I'll change it later
+			
+		elif chooseData["start_time"] == "final":
+			future = datetime.timedelta(days = 15) # I'll change it later
+			
+		chooseData["return_time"] = now + future
+			
+		
+	else:
+		chooseData["start_time"] = None	
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
+# start the webserver
+if __name__ == "__main__":
+	app.debug = True
+	
+	port = int(os.environ.get('PORT', 5000)) # locally PORT 5000, Heroku will assign its own port
+	app.run(host='0.0.0.0', port=port)
+
+
+
+	
